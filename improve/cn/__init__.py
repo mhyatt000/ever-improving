8432be322a0c2dc37c3d7c9bcf288216055a6a28
Matt Hyatt <mhyatt000@gmail.com>
2024-07-11 18:03:24 -0500

    new config nodes

--- improve/cn/__init__.py
+++ improve/cn/__init__.py
@@ -1,4 +1,23 @@
+from improve.util.config import default
+from dataclasses import dataclass
+from typing import Any, List
+from omegaconf import MISSING
+
 from .algo import *
-from .foundation import *
 from .buffer import base
 from .buffer.base import Buffer
+from .env.foundation.base import *
+
+defaults = [
+    # Load the config "mysql" from the config group "db"
+    {"db": "mysql"}
+]
+
+
+@dataclass
+class Config:
+    # this is unfortunately verbose due to @dataclass limitations
+    defaults: List[Any] = default(defaults)
+
+    # Hydra will populate this field based on the defaults list
+    db: Any = MISSING
---
+++ improve/cn/env/foundation/base.py
@@ -0,0 +1,92 @@
+import os.path as osp
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional, Union
+
+from improve.util.config import default, store
+
+
+class Strategy(Enum):
+    CLIP = "clip"
+    DYNAMIC = "dynamic"
+    NONE = None
+
+
+@dataclass
+class FoundationModel:
+    name: str = None
+    ckpt: Optional[str] = None
+    task: str = None
+
+    noact: list = default([-1, -2, -3, -4])  # no action dimensions
+    strategy: Strategy = Strategy.CLIP # residual scaling strategy
+    residual_scale: float = 1.0  # residual scaling factor
+
+    batch_size: int = 8  # number of parallel environments
+
+    def __post_init__(self):
+
+        # follow simpler.OctoInference
+        self.policy = self.name
+        if self.policy in ["octo-base", "octo-small"]:
+            if self.ckpt in [None, "None"] or "rt_1_x" in self.ckpt:
+                self.ckpt = self.policy
+
+        # follow simpler.OctoInference
+        # build policy
+        if "google_robot" in self.task:
+            self.policy_setup = "google_robot"
+        elif "widowx" in self.task:
+            self.policy_setup = "widowx_bridge"
+        else:
+            raise NotImplementedError()
+
+
+@store
+@dataclass
+class OctoS(FoundationModel):
+    name: str = "octo-small"
+    ckpt: Optional[str] = None
+    task: str = "widowx_put_eggplant_in_basket"
+
+
+@store
+@dataclass
+class OctoB(FoundationModel):
+    name: str = "octo-base"
+    ckpt: Optional[str] = None
+    task: str = "widowx_put_eggplant_in_basket"
+
+
+@dataclass
+class RT1Model:
+
+    num_image_tokens: int = 81
+    num_action_tokens: int = 11
+    layer_size: int = 256
+    vocab_size: int = 512
+
+    # Use token learner to reduce tokens per image to 81.
+    use_token_learner: bool = True
+    # RT-1-X uses (-2.0, 2.0) instead of (-1.0, 1.0).
+    world_vector_range: tuple = (-2.0, 2.0)
+
+
+@store
+@dataclass
+class RTX(FoundationModel):
+    name: str = "rtx"
+    task: str = "google_robot_pick_horizontal_coke_can"
+
+    model: RT1Model = default(RT1Model())
+    # checkpoint_path: str = "rt_1_x_jax/b321733791_75882326_000900000"
+    ckpt: str = "/home/zero-shot/rt_1_x_jax/b321733791_75882326_000900000"
+    seqlen: int = 15
+    batch_size: int = 2
+
+    cached: bool = True
+
+    # def __post_init__(self): self.checkpoint_path = osp.join(improve.WEIGHTS, self.checkpoint_path)
+    def __post_init__(self): 
+        super().__post_init__()
+        self.model = RT1Model(**self.model)
